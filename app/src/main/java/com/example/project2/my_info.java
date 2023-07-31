b429276a8c6b438c14080b3067c80687922381a2
이승희 <daalgwabyul@naver.com>
2023-07-31 20:49:19 +0900

    layout_1

---
+++ app/src/main/java/com/example/project2/my_info.java
@@ -0,0 +1,22 @@
+package com.example.project2;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+public class my_info {
+}
